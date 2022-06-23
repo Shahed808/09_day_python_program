a1d2af69b7c62227e9ed2049114955dc0a668094
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-06-23 10:55:51 +0530

    Add files via upload

---
+++ slicing_part_strings.py
@@ -0,0 +1,34 @@
+''' slicing part on strings '''
+
+# r = "programmer"
+# print(r[:5])          # progr
+# print(r[1:2])          # r
+# print(r[1:])          # rogrammer
+# print(r[:2])          # pr
+
+
+# t = "Iron man"
+# print(t[:11])     # Iron man
+# print(t[7:8])    # n
+# print(t[4:4])     # Empty line
+
+
+# g = "NewYork"
+# print(g[0:5])  # New Yo
+# print(g[0:9])   # New York
+# print(g[:0])     # Empty line
+# print(g[3:2])       # Empty line 
+
+
+d = "Developer"
+for x in d: 
+ print(d[:1])
+ print(d[:2])
+ print(d[:3])
+ print(d[:4])
+ print(d[:5])
+ print(d[:6])
+ print(d[:7])
+ print(d[:8])
+ print(d[:9])
+
