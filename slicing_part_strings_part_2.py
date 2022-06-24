bf0d5b58baee63c6a34a0ea27c118a96b3e37d39
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-06-24 13:09:48 +0530

    Add files via upload

---
+++ slicing_part_strings_part_2.py
@@ -0,0 +1,58 @@
+''' slicing part with neagtive indexes'''
+# s = "innovations"
+# print(s[-1:])  # s
+# print(s[-2:])   # ns
+# print(s[-3:])    # ons
+# print(s[-4:])   # ions
+# print(s[-5:])     # tions
+
+
+# d = "environment"
+# print(d[:-5])    # enviro
+# print(d[-8:-1])    # ironmen
+# print(d[-7:-1])    # ronmen
+# print(d[-6:-1])    # onmen
+# print(d[-5:-1])    # nmen
+
+
+
+# r = "programmer"
+# print(r[-1:5])        # Blankline
+# print(r[5:-1])        # amme
+# print(r[4:-3])        # ram
+
+
+''' slicing with double colon'''
+
+# p = "Engineering"
+# print(p[::])  # Engineering
+# print(p[::-2])  # Eeg
+# print(p[::-1])  # gnireenignE
+# print(p[::-4])  # geg
+# print(p[::-6])  # gn
+# print(p[::-9])   # gn
+
+
+# v = "coding"
+# print(v[-1::])  #g
+# print(v[-1:-5:2])  # Blankline
+# print(v[-6:-1:3])  # ci
+# print(v[-5:-1:4])  #o
+
+
+
+# t = "software"
+# print(t[7:1:-1]) # Doubt session
+
+
+
+
+
+
+
+
+
+
+
+
+
